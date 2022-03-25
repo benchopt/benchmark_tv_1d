ae5f0a30ef87b668f8c42a099f8983f5fde126d1
EnLAI111 <102022158+EnLAI111@users.noreply.github.com>
2022-03-25 14:58:18 +0100

    Add files via upload

--- objective.py
+++ objective.py
@@ -1,22 +1,22 @@
+import numpy as np
 from benchopt import BaseObjective
 
 
 class Objective(BaseObjective):
     name = "Ordinary Least Squares"
 
-    parameters = {
-        'fit_intercept': [False],
-    }
+    parameters = {'fit_intercept': [False]}
 
     def __init__(self, fit_intercept=False):
         self.fit_intercept = fit_intercept
 
-    def set_data(self, X, y):
-        self.X, self.y = X, y
+    def set_data(self, reg, y):
+        self.reg, self.y = reg, y
 
-    def compute(self, beta):
-        diff = self.y - self.X.dot(beta)
-        return .5 * diff.dot(diff)
+    def compute(self, x):
+        A = np.eye(len(self.y), dtype=int)
+        R = self.y - A @ x
+        return .5 * R @ R + self.reg*(abs(np.diff(x)).sum())
 
     def to_dict(self):
-        return dict(X=self.X, y=self.y, fit_intercept=self.fit_intercept)
+        return dict(reg=self.reg, y=self.y)
