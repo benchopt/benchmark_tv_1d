96a5a6e8f3097fa1ae3de1a5935c720d76790e87
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2022-03-29 21:40:15 +0200

    fix?

--- solvers/python-PGD.py
+++ solvers/python-PGD.py
@@ -1,10 +1,16 @@
-import prox_tv as ptv
 from benchopt import BaseSolver
+from benchopt import safe_import_context
+
+with safe_import_context() as import_ctx:
+    import prox_tv as ptv
 
 
 class Solver(BaseSolver):
     """Gradient descent solver, optionally accelerated."""
     name = 'PGD'
+
+    install_cmd = 'conda'
+    requirements = ['pip:prox-tv']
 
     # any parameter defined here is accessible as a class attribute
 
--- test_config.py
+++ test_config.py
@@ -10,4 +10,5 @@ def check_test_solver_install(solver_class):
     particular architecture, call pytest.xfail when
     detecting the situation.
     """
-    pass
+    if solver_class.name.lower() == 'pgd' and sys.platform == 'darwin':
+        pytest.xfail('prox_tv is not easy to install on macos')
