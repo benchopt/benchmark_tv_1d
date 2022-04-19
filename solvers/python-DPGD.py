d630fee978d4889f56e4680e282ebb7f67ba07dd
EnLAI111 <ensophial@gmail.com>
2022-04-19 17:44:17 +0200

    parameter

--- solvers/python-DPGD.py
+++ solvers/python-DPGD.py
@@ -12,7 +12,7 @@ class Solver(BaseSolver):
     name = 'DPGD'
 
     stopping_criterion = SufficientProgressCriterion(
-        patience=10, strategy='callback'
+        patience=20, strategy='callback'
     )
 
     # any parameter defined here is accessible as a class attribute
