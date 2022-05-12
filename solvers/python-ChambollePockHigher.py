f0647283b77ce6c620c9b659c2b82d46ab7fe8be
Samuel Vaiter <samuel.vaiter@gmail.com>
2022-05-12 14:28:26 +0200

    Implement Huber+TV with CP

--- solvers/python-ChambollePockHigher.py
+++ solvers/python-ChambollePockHigher.py
@@ -20,11 +20,6 @@ class Solver(BaseSolver):
     parameters = {'ratio': [1.],
                   'theta': [1.]}
 
-    def skip(self, A, reg, y, c, delta, data_fit):
-        if data_fit == 'huber':
-            return True, "solver does not work with huber loss"
-        return False, None
-
     def set_objective(self, A, reg, y, c, delta, data_fit):
         self.reg = reg
         self.A, self.y = A, y
@@ -34,11 +29,11 @@ class Solver(BaseSolver):
 
     def run(self, callback):
         # Block preconditioning (2x2)
-        L = 2.0 # Lipschitz constant associated to D (only for 1d!!)
-        Lh = np.linalg.norm(self.A, ord=2)**2
-        tau = self.ratio / (L + np.sqrt(Lh))
-        sigma_v = 1. / (self.ratio * L)
-        sigma_w = 1. / (self.ratio * np.sqrt(Lh))
+        LD = 2.0 # Lipschitz constant associated to D (only for 1d!!)
+        LA = np.linalg.norm(self.A, ord=2)**2
+        tau = self.ratio / (LA + LD)
+        sigma_v = 1. / (self.ratio * LD)
+        sigma_w = 1. / (self.ratio * LA)
         # Init variables
         n, p = self.A.shape
         u = np.zeros(p)
@@ -49,7 +44,11 @@ class Solver(BaseSolver):
         while callback(u):
             u_old = u
             v = np.clip(v + sigma_v * np.diff(u_bar), -self.reg, self.reg)
-            w = (w + sigma_w * (self.A @ u_bar - self.y)) / (1. + sigma_w)
+            w_tmp = w + sigma_w * self.A @ u_bar
+            if self.data_fit == 'huber':
+                w = w_tmp - sigma_w * (self._prox_huber(w_tmp/sigma_w - self.y, 1./sigma_w) + self.y)
+            else:
+                w = (w_tmp - sigma_w *  self.y) / (1. + sigma_w)
             # grad.T = -div, hence + sign
             u = u + tau * np.diff(v, prepend=0, append=0) - tau * self.A.T @ w
             u_bar = u + self.theta * (u - u_old)
@@ -57,3 +56,7 @@ class Solver(BaseSolver):
 
     def get_result(self):
         return self.u
+
+    def _prox_huber(self, u, mu):
+        return np.where(np.abs(u) <= self.delta * (mu + 1.),
+                        u / (mu + 1.), u - self.delta * mu * np.sign(u))
