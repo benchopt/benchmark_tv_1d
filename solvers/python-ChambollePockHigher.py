aac7daaff69923f35976cc38b0606a3cd8579c3e
Samuel Vaiter <samuel.vaiter@gmail.com>
2022-05-12 14:51:02 +0200

    flake8 was yelling at me

--- solvers/python-ChambollePockHigher.py
+++ solvers/python-ChambollePockHigher.py
@@ -4,21 +4,20 @@ from benchopt import safe_import_context
 
 with safe_import_context() as import_ctx:
     import numpy as np
-    from scipy.sparse import spdiags
 
 
 class Solver(BaseSolver):
-    """A first-order primal-dual algorithm for synthesis formulation."""
-    name = 'ChambollePock (higher dualization) analysis'
+    """A first-order primal-dual algorithm for analysis formulation."""
+
+    name = "Chambolle-Pock (higher dualization) analysis"
 
     stopping_criterion = SufficientProgressCriterion(
-        patience=20, strategy='callback'
-    )
-
+        patience=20,
+        strategy="callback")
 
     # any parameter defined here is accessible as a class attribute
-    parameters = {'ratio': [1.],
-                  'theta': [1.]}
+    parameters = {"ratio": [1.0],
+                  "theta": [1.0]}
 
     def set_objective(self, A, reg, y, c, delta, data_fit):
         self.reg = reg
@@ -29,15 +28,15 @@ class Solver(BaseSolver):
 
     def run(self, callback):
         # Block preconditioning (2x2)
-        LD = 2.0 # Lipschitz constant associated to D (only for 1d!!)
+        LD = 2.0  # Lipschitz constant associated to D (only for 1d!!)
         LA = np.linalg.norm(self.A, ord=2)
         tau = self.ratio / (LA + LD)
-        sigma_v = 1. / (self.ratio * LD)
-        sigma_w = 1. / (self.ratio * LA)
+        sigma_v = 1.0 / (self.ratio * LD)
+        sigma_w = 1.0 / (self.ratio * LA)
         # Init variables
         n, p = self.A.shape
         u = np.zeros(p)
-        v = np.zeros(p - 1) # we consider non-cyclic finite difference
+        v = np.zeros(p - 1)  # we consider non-cyclic finite difference
         w = np.zeros(n)
         u_bar = u
 
@@ -45,10 +44,14 @@ class Solver(BaseSolver):
             u_old = u
             v = np.clip(v + sigma_v * np.diff(u_bar), -self.reg, self.reg)
             w_tmp = w + sigma_w * self.A @ u_bar
-            if self.data_fit == 'huber':
-                w = w_tmp - sigma_w * (self._prox_huber(w_tmp/sigma_w - self.y, 1./sigma_w) + self.y)
+            if self.data_fit == "huber":
+                # Use Moreau identity + translation rule
+                prox_out = self._prox_huber(
+                    w_tmp / sigma_w - self.y, 1.0 / sigma_w
+                )
+                w = w_tmp - sigma_w * (prox_out + self.y)
             else:
-                w = (w_tmp - sigma_w *  self.y) / (1. + sigma_w)
+                w = (w_tmp - sigma_w * self.y) / (1.0 + sigma_w)
             # grad.T = -div, hence + sign
             u = u + tau * np.diff(v, prepend=0, append=0) - tau * self.A.T @ w
             u_bar = u + self.theta * (u - u_old)
@@ -58,5 +61,8 @@ class Solver(BaseSolver):
         return self.u
 
     def _prox_huber(self, u, mu):
-        return np.where(np.abs(u) <= self.delta * (mu + 1.),
-                        u / (mu + 1.), u - self.delta * mu * np.sign(u))
+        return np.where(
+            np.abs(u) <= self.delta * (mu + 1.0),
+            u / (mu + 1.0),
+            u - self.delta * mu * np.sign(u),
+        )
