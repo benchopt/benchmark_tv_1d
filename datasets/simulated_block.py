741d0a2aeff2c1783a60d7702c8e483f547fc017
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2022-05-25 10:06:09 +0200

    Lieanr operator

--- datasets/simulated_block.py
+++ datasets/simulated_block.py
@@ -1,9 +1,11 @@
 from benchopt import BaseDataset
 from benchopt import safe_import_context
+from numpy import transpose
 
 with safe_import_context() as import_ctx:
     import numpy as np
     from scipy.sparse import random as sprandom
+    from scipy.sparse.linalg import LinearOperator
 
 
 class Dataset(BaseDataset):
@@ -18,7 +20,8 @@ class Dataset(BaseDataset):
         'mu': [0],
         'n_features': [500],
         'n_samples': [400],
-        'type_A': ['identity', 'random'],
+        'type_A': ['conv'],
+        # 'type_A': ['identity', 'random', 'conv'],
         'num_block': [10]}
 
     def __init__(self, mu=0, sigma=0.3, n_features=10, n_samples=5,
@@ -33,20 +36,39 @@ class Dataset(BaseDataset):
 
     def set_A(self, rng):
         len_A = self.n_features - self.n_samples + 1
-        if len_A == 1 and self.type_A == 'identity':
-            A = np.array([1])
-        else:
-            A = rng.randn(len_A)
+        # if len_A == 1 and self.type_A == 'identity':
+        #     A = np.array([1])
+        # else:
+        #     A = rng.randn(len_A)
+        if self.type_A == 'identity':
+            A = LinearOperator(
+                dtype=np.float64,
+                matvec=lambda x: x,
+                matmat=lambda X: X,
+                shape=(self.n_samples, self.n_features),
+            )
+        elif self.type_A == 'conv':
+            filt = rng.randn(len_A)
+            A = LinearOperator(
+                dtype=np.float64,
+                matvec=lambda x: np.convolve(filt, x, mode='full'),
+                matmat=lambda X: np.array(
+                    [np.convolve(filt, x, mode='full') for x in X.T]
+                ).T,
+                shape=(self.n_samples, self.n_features),
+                rmatvec=lambda x: np.correlate(filt, x, mode='valid'),
+                rmatmat=lambda X: np.array(
+                    [np.correlate(filt, x, mode='valid') for x in X.T]
+                ).T
+            )
         return A
 
     def get_data(self):
         rng = np.random.RandomState(self.random_state)
-        w = np.cumsum(rng.randn(self.n_samples) * sprandom(1, self.n_samples,
-                      density=self.num_block/self.n_samples,
+        w = np.cumsum(rng.randn(self.n_features) * sprandom(1, self.n_features,
+                      density=self.num_block/self.n_features,
                       random_state=rng, data_rvs=np.ones).A[0])
         A = self.set_A(rng)
-        y = np.convolve(w, A) + rng.normal(self.mu, self.sigma,
-                                           self.n_features)
+        y = A @ w + rng.normal(self.mu, self.sigma, self.n_samples)
         data = dict(A=A, y=y)
-
         return data
--- solvers/python-ChambollePockPDsplit.py
+++ solvers/python-ChambollePockPDsplit.py
@@ -4,6 +4,7 @@ from benchopt import safe_import_context
 
 with safe_import_context() as import_ctx:
     import numpy as np
+    import scipy.sparse.linalg as splinalg
 
 
 class Solver(BaseSolver):
@@ -30,8 +31,10 @@ class Solver(BaseSolver):
     def run(self, callback):
         # Block preconditioning (2x2)
         LD = 2.0  # Lipschitz constant associated to D (only for 1d!!)
-        LA = np.linalg.norm(np.convolve(
-                np.ones(self.n_samples), self.A), ord=2)
+        if isinstance(self.A, splinalg.LinearOperator):
+            LA = splinalg.norm(self.A, ord=2)
+        else:
+            LA = np.linalg.norm(self.A, ord=2)
         tau = self.ratio / (LA + LD)
         sigma_v = 1.0 / (self.ratio * LD)
         sigma_w = 1.0 / (self.ratio * LA)
@@ -47,7 +50,8 @@ class Solver(BaseSolver):
             u_old = u
             v = np.clip(v + sigma_v * np.diff(u_bar),
                         -self.reg, self.reg)
-            w_tmp = w + sigma_w * np.convolve(u_bar, self.A)
+            w_tmp = w + sigma_w * (self.A @ u_bar)
+            # w_tmp = w + sigma_w * np.convolve(u_bar, self.A)
             if self.data_fit == "huber":
                 # Use Moreau identity + translation rule
                 prox_out = self._prox_huber(
@@ -57,8 +61,7 @@ class Solver(BaseSolver):
             else:
                 w = (w_tmp - sigma_w * self.y) / (1.0 + sigma_w)
             # grad.T = -div, hence + sign
-            u = u + tau * np.diff(v, prepend=0, append=0) - \
-                tau * np.correlate(w, self.A, mode="valid")
+            u = u + tau * np.diff(v, prepend=0, append=0) - tau * self.A.T @ w
             u_bar = u + self.theta * (u - u_old)
         self.u = u
 
