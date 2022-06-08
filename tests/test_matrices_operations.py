406fdc71a08c895884836b88d43184417387cde6
EnLAI111 <ensophial@gmail.com>
2022-06-08 11:08:25 +0200

    fix test

--- tests/test_matrices_operations.py
+++ tests/test_matrices_operations.py
@@ -1,6 +1,13 @@
 import numpy as np
+from scipy.sparse import spdiags
+from solvers.DPGD import get_inverse_D
 
 
-def test_inverse(D, D_inv):
-    n = D.shape[0]
-    return np.allclose(D @ D_inv, np.identity(n))
+def test_inverse_D():
+    rng = np.random.RandomState(42)
+    p = rng.randint(2, 100)
+    data = np.array([-np.ones(p), np.ones(p)])
+    diags = np.array([0, 1])
+    D = spdiags(data, diags, p-1, p)
+    D_inv = get_inverse_D(p)
+    np.testing.assert_almost_equal(D @ D_inv, np.identity(p-1))
