6196f87f4dd4e6bd5e67d5991419f986009a48be
En LAI <102022158+EnLAI111@users.noreply.github.com>
2022-07-20 01:04:53 +0200

    CondatVu simplify (#45)

--- solvers/CondatVu.py
+++ solvers/CondatVu.py
@@ -33,8 +33,8 @@ class Solver(BaseSolver):
         # Block preconditioning (2x2)
         LD = 2.0  # Lipschitz constant associated to D (only for 1d!!)
         LA = get_l2norm(self.A)
-        sigma_v = 1.0 / (self.ratio * LD)
-        tau = 1 / (LA ** 2 / 2 + sigma_v * LD ** 2)
+        sigma = 1.0 / (self.ratio * LD)
+        tau = 1 / (LA ** 2 / 2 + sigma * LD ** 2)
         eta = self.eta
         # initialisation
         u = self.c * np.ones(p)
@@ -43,10 +43,8 @@ class Solver(BaseSolver):
         while callback(u):
             u_tmp = (u - tau * self.grad(self.A, u)
                      - tau * (-np.diff(v, append=0, prepend=0)))
-            v_tmp = (v + sigma_v * np.diff(2 * u_tmp - u)
-                     - sigma_v * st(v / sigma_v +
-                                    np.diff(2 * u_tmp - u),
-                                    self.reg / sigma_v))
+            v_tmp = np.clip(v + sigma * np.diff(2 * u_tmp - u),
+                            -self.reg, self.reg)
             u = eta * u_tmp + (1 - eta) * u
             v = eta * v_tmp + (1 - eta) * v
         self.u = u
