a64db92cc91f10bd57816ebc9d497a1859149cf7
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-07-10 23:09:32 +0200

    feat: add the mm prox as option for pgd.

--- solvers/PGD.py
+++ solvers/PGD.py
@@ -1,3 +1,4 @@
+from functools import partial
 from benchopt import BaseSolver
 from benchopt.stopping_criterion import SufficientProgressCriterion
 from benchopt import safe_import_context
@@ -7,6 +8,7 @@ with safe_import_context() as import_ctx:
     import prox_tv as ptv
     from benchmark_utils.shared import grad_huber
     from benchmark_utils.shared import get_l2norm
+    from benchmark_utils.tv_numba import tv_mm, jit_module, prox_condat
 
 
 class Solver(BaseSolver):
@@ -16,7 +18,7 @@ class Solver(BaseSolver):
     install_cmd = 'conda'
 
     # We need blas devel to get the include file for BLAS/LAPACK operations
-    requirements = ["blas-devel", 'pip:prox-tv']
+    requirements = ['pip:prox-tv', 'pip:numba', 'pip:numpy']
 
     stopping_criterion = SufficientProgressCriterion(
         patience=3, strategy='callback'
@@ -24,7 +26,8 @@ class Solver(BaseSolver):
 
     # any parameter defined here is accessible as a class attribute
     parameters = {'alpha': [1.],
-                  'use_acceleration': [False, True]}
+                  'use_acceleration': [False, True],
+                  'prox_op': ["condat_C", "tv_mm", "condat_numba"]}
 
     def set_objective(self, A, reg, y, c, delta, data_fit):
         self.reg = reg
@@ -33,7 +36,18 @@ class Solver(BaseSolver):
         self.delta = delta
         self.data_fit = data_fit
 
+
+
     def run(self, callback):
+        if self.prox_op == "condat_C":
+            prox_op = partial(ptv.tv1_1d, method='condat')
+        elif self.prox_op == "tv_mm":
+            jit_module()
+            prox_op = partial(tv_mm, max_iter=1000, tol=1e-6)
+        elif self.prox_op == "condat_numba":
+            jit_module()
+            prox_op = prox_condat
+
         p = self.A.shape[1]
         # alpha / rho
         stepsize = self.alpha / get_l2norm(self.A)**2
@@ -49,9 +63,9 @@ class Solver(BaseSolver):
                 t_new = (1 + np.sqrt(1 + 4 * t_old ** 2)) / 2
                 u_old[:] = u
                 u[:] = u_acc
-            u = ptv.tv1_1d(
+            u = prox_op(
                 u - stepsize * self.grad(self.A, u),
-                self.reg * stepsize, method='condat')
+                self.reg * stepsize)
             if self.use_acceleration:
                 u_acc[:] = u + (t_old - 1.) / t_new * (u - u_old)
         self.u = u
